669c30167f798cce189377b01983252aa0d07b82
khh090630-lang <khh090630@gmail.com>
2026-01-17 20:04:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -111,8 +111,8 @@ if st.session_state.run_nav and start_coords and end_coords:
         dest_node = end_edge[0] if get_dist(end_edge[0], start_coords) < get_dist(end_edge[1], start_coords) else end_edge[1]
 
         # 3. 가중치 설정
-        DETECTION_RADIUS = 0.0002  
-        PENALTY = 50               
+        DETECTION_RADIUS = 0.0001  
+        PENALTY = 100000               
         for u, v, k, data in G.edges(keys=True, data=True):
             data['my_weight'] = data['length']
             if 'geometry' in data: edge_geom = data['geometry']
@@ -165,4 +165,5 @@ if st.session_state.run_nav and start_coords and end_coords:
         
     except Exception as e:
         st.error(f"경로를 찾을 수 없습니다: {e}")
+
 
